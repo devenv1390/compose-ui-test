89924044e382b7d7171a35739f68b6106cb5b8db
light <864587620@qq.com>
2023-01-30 21:12:34 +0800

    Init Commit

---
+++ compose-ui-test-master/app/src/main/java/com/example/myapplication/test.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import java.lang.reflect.Modifier
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun test(){
+    Scaffold(
+        topBar = { Text(text = "123")}
+    ) {
+        Box(){
+            Text(text = "hello")
+        }//这是一次测试123123
+    }
+}
